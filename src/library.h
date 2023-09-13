ac22446d5d44df8996680c1f9814d2c1d9e91fae
Guido Biotti <biottiguido@gmail.com>
2023-09-13 13:00:09 -0300

    Agrego funciones para leer el string de numeros

--- src/library.h
+++ src/library.h
@@ -3,3 +3,50 @@
 void saludar(){
     printf("Hola mundo");
 }
+
+void leer_teclado(){
+    char string[100];
+    int i,j,k=0;
+    char numero[20];
+    long array[20];
+    printf("Introduce una cadena de numeros separados por '$': ");
+    scanf("%s", string);
+    while(string[i] != '\0'){
+        if(string[i] == '$'){
+            k=0;
+            array[j] = numero;
+            j++;
+        }
+        numero[k] = string[i];
+        k++;
+        i++;
+    }
+
+}
+
+void leer_archivo(){
+    FILE *archivo;
+    char string[100];
+    int i,j,k=0;
+    char numero[20];
+    long array[20];
+    archivo = fopen("numeros.txt", "r");
+    if(archivo == NULL){
+        printf("Error al abrir el archivo");
+    }else{
+        while(fgets(string, 100, (FILE*) archivo)){
+            while(string[i] != '\0'){
+                if(string[i] == '$'){
+                    k=0;
+                    array[j] = numero;
+                    j++;
+                }
+                numero[k] = string[i];
+                k++;
+                i++;
+            }
+        }
+    }
+    fclose(archivo);
+
+}
