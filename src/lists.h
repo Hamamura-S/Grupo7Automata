19b302b5bb801003f5de8f86c77c401852cb5b13
Santiago <hamamurasantiago@frba.utn.edu.ar>
2023-09-16 12:45:45 -0300

    mejora de funciones de listas

--- src/lists.h
+++ src/lists.h
@@ -19,25 +19,25 @@ int listSize(List lista);
 
 void push(List *lista, int data);
 
-    EJEMPLO: push(&operadores, 5);
+    EJEMPLO: push(operadores, 5);
         se agrega el 5 al final de la lista operadores
 
 
 int pop(List *lista);
 
-    EJEMPLO: int n = pop(&operadores);
+    EJEMPLO: int n = pop(operadores);
         n tiene el valor del ultimo operador almacenado
 
 
 void enqueue(List *lista, int data);
 
-    EJEMPLO: enqueue(&operadores, 5);
+    EJEMPLO: enqueue(operadores, 5);
         se agrega el 5 al comienzo de la lista operadores
 
 
 int dequeue(List* lista);
 
-    EJEMPLO: int n = dequeue(&operadores);
+    EJEMPLO: int n = dequeue(operadores);
         n tiene el valor del primer operador almacenado
 
 
@@ -46,81 +46,70 @@ NO SE REQUIRIERON FUNCIONES DE ACCESO POR INDICE
 */
 
 
-struct Node{
+typedef struct tNode{
     int value;
-    struct Node *next;
-};
+    struct tNode *next;
+    struct tNode *previous;
+}Node;
 
-typedef struct Node Node;
+typedef struct {
+    Node* head;
+    Node* first;
+    int size;
+}List;
 
-typedef Node* List;
-
-//asigna memoria dinamica para guardar un nuevo nodo
+//Devuelve un nuevo nodo en heap
 Node* createNode(int data){
 
     Node * newNode = (Node*)malloc(sizeof(Node));
 
     newNode->next=NULL;
+    newNode->previous=NULL;
     newNode->value=data;
 
     return newNode;
 
 }
 
-//Crea una lista vacia
-List createList(){
-    List newList=NULL;
+//Reserva heap para una lista vacia y la devuelve
+List* createList(){
+    List *newList=malloc(sizeof(List));
+    newList->first=NULL;
+    newList->head=NULL;
+    newList->size=0;
     return newList;
 }
 
-int listSize(List lista){
+//devuelve el tamanio de la lista
+int listSize(List* lista){
 
-    if(lista==NULL){
-        //lista no inicializada, no hay nodos
-        return 0;
-    }else{
-        //hay al menos 1 nodo, aumento por cada iteracion
-        int count=1;
-        Node* nodo = lista;
-        while(nodo->next!=NULL){
-            nodo=nodo->next;
-            count++;
-        }
-        return count;
-    }
+    return lista->size;
 
 }
 
-Node* getNode(List lista, int index){
+Node* getNode(List* lista, int index){
 
-    if(lista==NULL){
-        //lista no inicializada, no hay nodos
+    //chequeo que el index sea valido
+    if( index>=listSize(lista) || index<0 ){
         return NULL;
-    }else{
-        Node* nodo = lista; 
-        //itero hasta el nodo en la posicion [index]
-        for(int i=0; i<index; i++){
-            
-            nodo=nodo->next;
-
-            //chequeo no haberme excedido del ultimo nodo
-            if(nodo==NULL){
-                return NULL;
-            }
-
-        }
-
-        return nodo;
     }
 
+    Node* askedNode=lista->first;
+
+    //recorro desde el primer nodo hasta el pedido
+    for(int i=0;i<index;i++){
+        askedNode=askedNode->next;
+    }
+
+    return askedNode;
 }
 
 //devuelve el ultimo nodo de la lista
-Node* lastNode(List lista){
+Node* lastNode(List *lista){
 
-    Node* last = getNode(lista, listSize(lista)-1);
+    Node* askedNode = lista->head;
 
-    return last;
+    return askedNode;
 }
 
 //asigna memoria dinamica, creando un nodo y agregandolo al final
@@ -128,54 +117,56 @@ void push(List *lista, int data){
 
     Node* newNode = createNode(data);
 
-    if(*lista==NULL){
-        //la lista esta declarada pero no inicializada
-        *lista=newNode;
-
+    if(listSize(lista)==0){
+        //si la lista esta vacia, el primer nodo es el nuevo
+        lista->first=newNode;
     }else{
-        //iterar hasta hallar el ultimo nodo
-        Node* last=lastNode(*lista);
-        last->next=newNode;
+        //sino, el nuevo nodo tiene un nodo anterior. Se lo asigna
+        newNode->previous=lista->head;
+        //y el ultimo de la cola tiene como nuevo siguiente al nuevo nodo
+        (lista->head)->next=newNode;
     }
-
+    //en ambos casos, el ultimo nodo pasa a ser el nuevo
+    lista->head=newNode;
+    lista->size++;
 }
 
 //quita el ultimo nodo de la lista y devuelve el valor almacenado
     //devuelve -1 si la lista estaba vacia
 int pop(List *lista){
-    
-    int length = listSize(*lista);
+   
+    int count=listSize(lista);    
 
-    if(*lista==NULL){
-        //la lista esta vacia
-
+    //me aseguro de que la lista no este vacia
+    if(count==0){
         return -1;
+    }
 
-    }else if(length==1){
-        //la lista solo tiene un elemento
+    int value;
 
-        int value=(*lista)->value;
-            //libero el nodo
-        free(*lista);
-        *lista=NULL;
-            //listo!
-        return value;
+    //si solo hay un elemento en la lista se elimina
+    if(count==1){
+
+        value=(lista->head)->value;
+        free(lista->head);
+        lista->first=NULL;
+        lista->head=NULL;
 
     }else{
-        //busco el ultimo
-        Node* last = lastNode(*lista);
-        //y busco el anteulitmo
-        Node* newLast = getNode(*lista, length-2);
 
-        //ahora puedo liberar el ultimo y actualizar el anteultimo
-        int value=last->value;
-        free(last);
-
-        newLast->next=NULL;
-
-        return value;
-
+        //sino, buscamos el ultimo
+        Node* popped = lastNode(lista);
+            //el nuevo ultimo es el previo al extraido
+        lista->head=popped->previous;
+        (lista->head)->next=NULL;
+            //se devuelve el valor del extraido
+        value=popped->value;
+        free(popped);
     }
+
+    lista->size--;
+    return value;
+
 }
 
 void enqueue(List *lista, int data){
@@ -183,37 +174,55 @@ void enqueue(List *lista, int data){
     Node* newNode = createNode(data);
 
     //hago que el siguiente al nuevo nodo sea el primero de la lista
-    newNode->next=*lista;
+    newNode->next=lista->first;
+
+    if(lista->first==NULL){
+        //la lista esta vacia
+        lista->head=newNode;
+    }else{
+        //la lista tiene al menos un elemento
+            //el previo al primero ahora es el nuevo
+        (lista->first)->previous=newNode;
+    }
 
     //y ajusto lista para que apunte al nuevo primer nodo
-    *lista=newNode;
+    lista->first=newNode;
 
+    lista->size++;
 }
 
 //devuelve el primer elemento
     //-1 si esta vacia
 int dequeue(List* lista){
 
-    if(*lista==NULL){
+    if(listSize(lista)==0){
         //la lista esta vacia
         return -1;
-
     }
 
-    Node* firstNode = *lista;
+    Node* firstNode = lista->first;
 
     //hago que lista apunte al segundo nodo
-    *lista=(*lista)->next;
+    lista->first=(lista->first)->next;
+
+    if(lista->first==NULL){
+        //si el primero llega a NULL, la lista ahora esta vacia
+        lista->head=NULL;
+    }
 
     //libero el primer nodo
     int value = firstNode->value;
     free(firstNode);
+
+    lista->size--;
+
     return value;
 
 }
 
 void freeList(List *lista){
-    while(*lista!=NULL){
+    while(listSize(lista)>0){
         dequeue(lista);
     }
+    free(lista);
 }
