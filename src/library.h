fa22b0deff53c2878cbae91ec54f4e4dcd455518
Guido Biotti <biottiguido@gmail.com>
2023-09-18 13:42:03 -0300

    Punto 2 V1.0

--- src/library.h
+++ src/library.h
@@ -95,3 +95,41 @@ int reconocerTipo(char palabra[], int len){
     return (state==1||state==5)?('d'):((state==3)?('h'):((state==4)?('o'):-1));
 
 }
+
+int reconocer_int(char numero){
+    switch (numero){
+        case '0':
+            return 0;
+            break;
+        case '1':
+            return 1;
+            break;
+        case '2':
+            return 2;
+            break; 
+        case '3':
+            return 3;
+            break; 
+        case '4':
+            return 4;
+            break; 
+        case '5':
+            return 5;
+            break; 
+        case '6':
+            return 6;
+            break; 
+        case '7':
+            return 7;
+            break; 
+        case '8':
+            return 8;
+            break; 
+        case '9':
+            return 9;
+            break; 
+        default:
+            return -1;
+            break;
+    }
+}
