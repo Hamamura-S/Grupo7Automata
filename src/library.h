fa3e446adc6febd2bff05e9fbc8f3c34c9d31950
Guido Biotti <biottiguido@gmail.com>
2023-09-18 12:22:30 -0300

    Punto 1 V1.0

--- src/library.h
+++ src/library.h
@@ -5,60 +5,83 @@
 #include "lists.h"
 #include "automata.h"
 
-long array[20];
-
 void saludar(){
     printf("Hola mundo\n");
 }
 
-void mostrar_array(long array[]){
-    int i;
-    for(i=0; i<20; i++){
-        printf("%ld", array[i]);
-    }
-}
-
-long* recorrer_string(char string[]){
-    char digitos[] = "0123456789xABCDEF";
-    char numero[20];
-    int i,j,k=0;
-    while(string[i] != '\0'){
-        if(strchr(digitos, string[i]) == NULL){
-            k=0;
-            array[j] = *numero;
-            j++;
-            array[j] = string[i];
-            j++;
-        }
-        numero[k] = string[i];
-        k++;
-        i++;
-    }
-    mostrar_array(array);
-    return array;
-}
-
-long leer_teclado(){
-    char string[100];
+char* leer_teclado(){
+    char *string_teclado[50] = malloc(sizeof(char)*50);
     printf("Introduce una cadena de numeros: ");
-    scanf("%s", string);
-    long *array = recorrer_string(string);
-    return array;
+    fgets(string_teclado, 50, stdin);
+    return string_teclado;
 }
 
-long leer_archivo(){
+char* leer_archivo(char nombre_archivo[]){
     FILE *archivo;
-    char string[100];
-    archivo = fopen("numeros.txt", "r");
+    char *string_archivo[50] = malloc(sizeof(char)*50);
+    int i=0; //longitud de la cadena
+    char c=0; //caracter a leer
+    archivo = fopen(nombre_archivo, "r");
     if(archivo == NULL){
         printf("Error al abrir el archivo");
-        return -1;
+        return (NULL);
     }
-    long *array = recorrer_string(string);
+    while((c=fgetc(archivo))!=EOF){
+        string_archivo[i]=c;
+        i++;
+    }
+    string_archivo[i]='\0';
     fclose(archivo);
-    return *array;
+    return *string_archivo;
 }
 
+//crear una funcion char* automata_uno que reciba un string de numeros (decimales con y sin signo, octales y hexadecimales) separados por el signo $. Separe cada numero hasta el $ y lo pase a la funcion reconocerTipo(char palabra[], int len) para saber si es decimal, octal o hexa y luego sume uno a un contador de cada tipo.
+
+char* automata_uno(){
+    printf("Queres leer un archivo o escribir en el teclado? (1/2)\n");
+    int opcion;
+    scanf("%d", &opcion);
+    char *string;
+    if(opcion==1){
+        printf("Introduce el nombre del archivo: ");
+        char nombre_archivo[50];
+        scanf("%s", nombre_archivo);
+        string = leer_archivo(nombre_archivo);
+    }
+    else{
+        string = leer_teclado();
+    }
+    int d,o,h,e=0;
+    char* errores_lexicos;
+    int i,j,k =0;
+    char *palabra;
+    while(i<strlen(string)){
+        if(string[i]=='$'){
+            j=0;
+            switch(reconocerTipo(palabra, strlen(palabra)-1)){
+                case 'd':
+                    d++;
+                    break;
+                case 'o':
+                    o++;
+                    break;
+                case 'h':
+                    h++;
+                    break;
+                default:
+                    e++;
+                    errores_lexicos[k]=palabra;
+                    k++;
+                    break;
+            }
+        }
+        while(string[i]!='$'){
+            palabra[j]=string[i];
+            j++;
+            i++;
+        }
+    }
+}
 
 //devuelve 'd': decimal, 'o': octal, 'h': hexadecimal o -1 para ninguno
 int reconocerTipo(char palabra[], int len){
